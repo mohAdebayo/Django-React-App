a199c2f24276f72b9a3ce66b0d53db8a2b2af992
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2023-01-25 20:40:30 +0100

    django proxy pass

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -18,7 +18,7 @@ export const login = (email, password) => async dispatch => {
     const body = JSON.stringify({ email, password });
 
     try {
-        const res = await axios.post(`http://20.124.61.232/api/token`, body, config);
+        const res = await axios.post(`http://20.124.61.232/api/token/`, body, config);
 
         dispatch({
             type: LOGIN_SUCCESS,
@@ -45,7 +45,7 @@ export const signup = ({ name, email, password, password2 }) => async dispatch =
     const body = JSON.stringify({ name, email, password, password2 }); 
 
     try {
-        const res = await axios.post(`http://20.124.61.232/api/accounts/signup`, body, config);
+        const res = await axios.post(`http://20.124.61.232/api/accounts/signup/`, body, config);
 
         dispatch({
             type: SIGNUP_SUCCESS,
