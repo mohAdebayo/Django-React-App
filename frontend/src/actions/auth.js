cbfdc1ba5e654764dad55046f6c43de30f09b5d7
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2023-01-28 19:40:01 +0100

    Update docker.yml

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -18,7 +18,7 @@ export const login = (email, password) => async dispatch => {
     const body = JSON.stringify({ email, password });
 
     try {
-        const res = await axios.post(`http://victorsapp.20.84.2.104.nip.io/api/token/`, body, config);
+        const res = await axios.post(`http://victorsapp.20.253.112.1.nip.io/api/api/token/`, body, config);
 
         dispatch({
             type: LOGIN_SUCCESS,
@@ -45,7 +45,7 @@ export const signup = ({ name, email, password, password2 }) => async dispatch =
     const body = JSON.stringify({ name, email, password, password2 }); 
 
     try {
-        const res = await axios.post(`http://victorsapp.20.84.2.104.nip.io/api/accounts/signup/`, body, config);
+        const res = await axios.post(`http://victorsapp.20.253.112.1.nip.io/api/api/accounts/signup/`, body, config);
 
         dispatch({
             type: SIGNUP_SUCCESS,
--- frontend/src/components/ListingForm.js
+++ frontend/src/components/ListingForm.js
@@ -33,7 +33,7 @@ const ListingForm = (props) => {
         };
 
         setLoading(true);
-        axios.post(`http://victorsapp.20.84.2.104.nip.io/api/listings/search`, { sale_type, price, bedrooms, home_type, bathrooms, sqft, days_listed, has_photos, open_house, keywords }, config)
+        axios.post(`http://victorsapp.20.253.112.1.nip.io/api/api/listings/search`, { sale_type, price, bedrooms, home_type, bathrooms, sqft, days_listed, has_photos, open_house, keywords }, config)
         .then(res => {
             setLoading(false);
             props.setListings(res.data);
