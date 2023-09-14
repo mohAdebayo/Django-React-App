32ba32bc28ba1ae21b273727a496f7979753d032
muhammad <mohadebayo05@gmail.com>
2023-09-14 08:14:28 -0700

    updated with localhost ipaddress

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -18,7 +18,7 @@ export const login = (email, password) => async dispatch => {
     const body = JSON.stringify({ email, password });
 
     try {
-        const res = await axios.post(`http://localhost:8000/api/token/`, body, config);
+        const res = await axios.post(`http://192.168.56.14:8000/api/token/`, body, config);
 
         dispatch({
             type: LOGIN_SUCCESS,
@@ -45,7 +45,7 @@ export const signup = ({ name, email, password, password2 }) => async dispatch =
     const body = JSON.stringify({ name, email, password, password2 }); 
 
     try {
-        const res = await axios.post(`http://localhost:8000/api/accounts/signup`, body, config);
+        const res = await axios.post(`http://192.168.56.14:8000/api/accounts/signup`, body, config);
 
         dispatch({
             type: SIGNUP_SUCCESS,
